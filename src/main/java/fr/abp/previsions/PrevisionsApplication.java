c47d90fe66c6332dc30a9b0b548f67fb978cac57
Anne <patesdegeek@gmail.com>
2021-03-19 10:27:42 +0100

    Initial commit

---
+++ src/main/java/fr/abp/previsions/PrevisionsApplication.java
@@ -0,0 +1,13 @@
+package fr.abp.previsions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrevisionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrevisionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/abp/previsions/PrevisionsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.abp.previsions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrevisionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
